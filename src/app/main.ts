a80a3f0b325480a788cf4dd58fbb1e4b707bdcb5
Keerthi Niranjan <keerthiniranjan16@gmail.com>
2020-07-21 00:59:00 +0530

    Window Initial design

---
+++ src/app/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        height: 80,
+        width: 1024,
+        resizable: false,
+        alwaysOnTop: true,
+        skipTaskbar: true,
+        frame: false,
+        titleBarStyle: 'hiddenInset',
+        backgroundColor: '#222831',
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../../index.html'));
+    // mainWindow.webContents.openDevTools();
+}
+
+app.on('ready', () => {
+    createWindow();
+
+    app.on('activate',  () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
